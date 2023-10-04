0272503997cdc4dd5a35030910cce505242ed09e
wzxrinaqwx <146193404+wzxrinaqwx@users.noreply.github.com>
2023-10-04 15:10:37 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -33,11 +33,33 @@ class Player(GameSprite):
 ball = GameSprite('ball.jpg', 200, 200, 10, 100, 100)
 player1 = Player('player1.png', 600, 100, 10, 100, 130)
 player2 = Player('player2.jpg', 0, 100, 10, 100, 130)
+
+speed_x = 3
+speed_y = 3
+finish = False
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('Player1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('Player2 LOSE!', True, (180, 0, 0))
 while run:
     for e in event.get():
         if e.type == QUIT:
                 run = False
     window.blit(background, (0, 0))
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+        speed_x *=-1
+    if ball.rect.x > win_width -50:
+        finish = True 
+        window.blit(lose2 ,(200, 200) )
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
     
    
     ball.reset()
@@ -49,7 +71,7 @@ while run:
     player2.update_l()
 
     display.update()
-    time.delay(50)
+    time.delay(10)
 
 
     
